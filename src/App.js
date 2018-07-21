be848e77e41ce8566c6e589b494f990789a49372
Erik <erikwjonsson@gmail.com>
2018-07-21 18:32:06 +0200

    Removed logo and some other unnecessary React boilerplate

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import SearchRoute from "./components/routes/SearchRoute";
 
@@ -12,12 +11,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Search drink events</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         {<SearchRoute />}
       </div>
     );
